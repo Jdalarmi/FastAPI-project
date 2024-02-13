bcfd11dc1e4c2cfc7f3b88467ea71265c898603b
Jeferson Dalarmi <121770443+Jdalarmi@users.noreply.github.com>
2024-02-13 07:19:47 -0300

    Conectar base de dados no postgreSQL

--- Todoapp/database.py
+++ Todoapp/database.py
@@ -1,11 +1,21 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+user=os.getenv('USER')
+password=os.getenv("PASSWORD")
+host=os.getenv("HOST")
+db=os.getenv("DB")
 
 
-SQLALCHEMY_DATABASE_URL = 'sqlite:///.todosapp.db'
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread': False})
+SQLALCHEMY_DATABASE_URL = f'postgresql://{user}:{password}@{host}/{db}'
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
